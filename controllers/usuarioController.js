b955a29d38024a0a73362a91085b2db30a14b9e2
Carlos Morales <240221@utxicotepec.edu.mx>
2026-02-26 09:38:00 -0600

    PC-BienesRaices - Sección 06
    update: Se actualizó las vistas y controlador del registro de usuarios para poder permitir la validación de los datos a través de express-validatior

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from "express-validator";
 import Usuario from "../models/Usuario.js";
 
 const formularioLogin = (req, res) => {
@@ -16,7 +17,24 @@ const registrarUsuario = async(req,res) =>
 {
     console.log("Intentando registrar a un usuario nuevo con los datos del formulario:");
     console.log(req.body);
-    const data =
+    
+    //Validacion de los datos del formulario previo a registro en la BD
+    // Definir reglas de validacion
+    await check('nombreUsuario').notEmpty().withMessage("El nombre de la persona no puede ser vacio").run(req);
+    await check('emailUsuario').notEmpty().withMessage("El correo electronico no puede ser vacio").isEmail().withMessage
+    ("el correo electrónico no tiene un formato adecuado").run(req)
+    await check('passwordUsuario').notEmpty().withMessage("La contraseña parece estar vacia").isLength({min:8, max:30}).withMessage
+    ("La longitud de la contraseña debe ser entre 8 y 30 caracteres").run(req);
+    await check('confirmacionUsuario').equals(req.body.passwordUsuario).withMessage("Ambas contraseñas deben ser iguales").run(req);
+
+    // Aplicamos las reglas definidas
+    let resultadoValidacion = validationResult(req);
+
+    // Validar si hay errores en la recepción de datos, si mandar a BD
+
+    if(resultadoValidacion.isEmpty())
+    {
+            const data =
     {
         name: req.body.nombreUsuario,
         email: req.body.emailUsuario,
@@ -24,6 +42,16 @@ const registrarUsuario = async(req,res) =>
     }
     const usuario = await Usuario.create(data);
     res.json(usuario)
+    }
+    else
+    res.render("auth/registro", {
+        pagina: "Error al intentar crear una cuenta",
+        errores: resultadoValidacion.array(),
+        usuario: {nombreUsuario: req.body.nombreUsuario,
+            emailUsuario: req.emailUsuario
+        
+        }});
 }
+
 export {
     formularioLogin, formularioRegistro,registrarUsuario, formulariorecuperacion}
